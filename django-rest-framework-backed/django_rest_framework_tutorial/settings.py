be0ef3d5d8d16c0bcc203a6dd5c5dcd2b2edc363
twtrubiks <twtrubiks@gmail.com>
2019-06-09 12:30:50 +0800

    CORS setting

--- django-rest-framework-backed/django_rest_framework_tutorial/settings.py
+++ django-rest-framework-backed/django_rest_framework_tutorial/settings.py
@@ -25,13 +25,36 @@ SECRET_KEY = 'g9je0wxn58$wico&t@@k6@1$yu)gv$cch7yz*9bin4&8$m@ulb'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True # corsheaders
 
 # CORS_ORIGIN_WHITELIST = (
 #     # 'google.com',
 #     # 'hostname.example.com',
 #     '127.0.0.1:8002',
 # )
+
+CORS_ALLOW_METHODS = [
+ #   'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Cache-Control'
+]
 
 # Application definition
 
@@ -50,7 +73,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware', # CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
